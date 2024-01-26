a13886370308b86bd4dc9b21ed358d8b86549dbe
Luis Torres <luistorreslt0101@gmail.com>
2024-01-25 19:08:43 -0600

    Cambios hechos el 25/01/2024

--- app/src/main/java/com/example/aplicacion1/MainActivity.java
+++ app/src/main/java/com/example/aplicacion1/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.aplicacion1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nombres, apellidos, telefono;
+    Button btnproceso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se han seteado los elementos de la interfaz grafica de usuario
+        nombres = (EditText) findViewById(R.id.nombres);
+        apellidos = (EditText) findViewById(R.id.apellidos);
+        telefono = (EditText) findViewById(R.id.telefono);
+        btnproceso = (Button) findViewById(R.id.btnprocesar);
+
+        //Evento del boton
+        btnproceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), nombres.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
